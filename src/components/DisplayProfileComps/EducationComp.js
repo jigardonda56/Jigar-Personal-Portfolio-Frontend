ca0eb3c5d4f4e1edeca537d45d126c90b2bfccb5
jigardonda56 <80046052+jigardonda56@users.noreply.github.com>
2023-04-22 10:09:43 -0400

    education visibility testing

--- src/components/DisplayProfileComps/EducationComp.js
+++ src/components/DisplayProfileComps/EducationComp.js
@@ -6,16 +6,16 @@ import '../../css/Education.css'
 const EducationComp = () => {
     const context = useContext(myContext);
     const { education, getEducation } = context;
-    
+
     useEffect(() => {
         getEducation();
     }, [])
-    
-    
+
+
     const [visibleClass, setvisibleClass] = useState("");
     let breakCondition = false;
     window.addEventListener('scroll', function () {
-        var element = document.querySelector('.education-text-div');
+        var element = document.querySelector('.education-main-div');
         var position = element.getBoundingClientRect();
         // checking for partial visibility
         if (position.top >= 0 && position.bottom <= window.innerHeight && !breakCondition) {
@@ -30,26 +30,29 @@ const EducationComp = () => {
 
     return (
         <>
-            <h2 className='education-h2'>Education</h2>
-            <div className='education-background-div'>
-                {
-                    education.map((data, index) => {
-                        return <div key={index} className={"card mb-3 education-main-div " + visibleClass}>
-                            <div className="education-div2">
-                                <div className="education-image-div">
-                                    <img src={data.image || blank_profile_pic} className="img-fluid rounded-start" alt="..." />
+            <div className="education-main-div">
+                <h2 className='education-h2'>Education</h2>
+                <div className='education-background-div'>
+                    {
+                        education.map((data, index) => {
+                            return <div key={index} className={"card mb-3 education-main-div " + visibleClass}>
+                                <div className="education-div2">
+                                    <div className="education-image-div">
+                                        <img src={data.image || blank_profile_pic} className="img-fluid rounded-start" alt="..." />
+                                    </div>
+                                    <div className="education-text-div">
+                                        <h5 className="card-title">{data.title}</h5>
+                                        <p className="card-text"><small className="text-muted">{data.subtitle}</small></p>
+                                        <p className="card-text"><small className="text-muted">{data.date}</small></p>
+                                        <pre className="card-text education-desc" style={{ whiteSpace: 'pre-wrap' }} >{data.description}</pre>
+                                    </div>
                                 </div>
-                                <div className="education-text-div">
-                                    <h5 className="card-title">{data.title}</h5>
-                                    <p className="card-text"><small className="text-muted">{data.subtitle}</small></p>
-                                    <p className="card-text"><small className="text-muted">{data.date}</small></p>
-                                    <pre className="card-text education-desc" style={{ whiteSpace: 'pre-wrap' }} >{data.description}</pre>
-                                </div>
-                            </div>
-                        </div>;
-                    })
-                }
+                            </div>;
+                        })
+                    }
+                </div>
             </div>
+
         </>
     )
 
