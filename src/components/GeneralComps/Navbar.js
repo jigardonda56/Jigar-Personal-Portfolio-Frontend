9ea68f8297fec55edb828e66100499720ab55f18
jigardonda56 <80046052+jigardonda56@users.noreply.github.com>
2023-04-23 18:02:05 -0400

    linkedin link adding

--- src/components/GeneralComps/Navbar.js
+++ src/components/GeneralComps/Navbar.js
@@ -23,38 +23,38 @@ const Navbar = () => {
                 <div className='navbar-title'>
                     <Link className="navbar-brand navbar-title-link" to="/">Jigar Donda</Link>
                 </div>
-             
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 navbar-ul">
-                        {/* nav items */}
-                        {/* <li className="nav-item">
+
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0 navbar-ul">
+                    {/* nav items */}
+                    {/* <li className="nav-item">
                             <Link className={`nav-link ${location.pathname === "/profile" ? "active" : ""}`} to="/profile">Profile</Link>
                         </li>
 
                         <li className="nav-item">
                             <Link className={`nav-link ${location.pathname === "/admin" ? "active" : ""}`} to="/admin">Admin</Link>
-                        </li>
+                        </li>*/}
 
-                        <li className="nav-item">
-                            <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About</Link>
-                        </li> */}
+                    <li className="nav-item">
+                        <Link className='nav-link' to={{ pathname: "https://www.linkedin.com/in/jigar-donda-71587b220/" }} target="_blank">LinkedIn</Link>
+                    </li>
 
-                        {localStorage.getItem('token') ? <li className="nav-item">
-                            <Link className={`nav-link ${location.pathname === "/admin" ? "active" : ""}`} to="/admin">Edit Profile</Link>
-                        </li>
-                            :
-                            <></>
-                        }
-                    </ul>
-                    <div className='email-div'>
-                        <a href="mailto:jigar.donda56@gmail.com"><img src={mailIcon} alt="" /> <span className="email-span"> jigar.donda56@gmail.com</span></a>
-                    </div>
-
-                    {localStorage.getItem('token') ? <form className="d-flex" role="search">
-                        <button className='btn btn-primary mx-3' onClick={logoutClick}>Logout</button> </form>
+                    {localStorage.getItem('token') ? <li className="nav-item">
+                        <Link className={`nav-link ${location.pathname === "/admin" ? "active" : ""}`} to="/admin">Edit Profile</Link>
+                    </li>
                         :
                         <></>
                     }
-                
+                </ul>
+                <div className='email-div'>
+                    <a href="mailto:jigar.donda56@gmail.com"><img src={mailIcon} alt="" /> <span className="email-span"> jigar.donda56@gmail.com</span></a>
+                </div>
+
+                {localStorage.getItem('token') ? <form className="d-flex" role="search">
+                    <button className='btn btn-primary mx-3' onClick={logoutClick}>Logout</button> </form>
+                    :
+                    <></>
+                }
+
             </div>
         </nav >
     )
